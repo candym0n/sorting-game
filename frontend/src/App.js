c3b19dc178e6f9a1f0d3c0c4dc0da140b09403eb
candym0n <jayden@bascio.com>
2025-01-31 11:16:22 +0800

    Added matrix effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,26 @@ import LevelSelectScreen from './level-select/LevelSelectScreen';
 import LevelScreen from './levels/LevelScreen';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Auth from './auth/AuthContext';
+import Rainfall from './Rainfall';
 
 function App() {
     return (
-        <Auth.Provider>
-            <Router>
-                <Routes>
-                    <Route path="/level-select" element={<LevelSelectScreen />} />
-                    <Route path="/level/:level" element={<LevelScreen />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/signout" element={<SignOut />} />
-                    <Route path="/delete-account" element={<Delete />} />
-                    <Route path="*" element={<Navigate to="/level-select" />} />
-                </Routes>
-            </Router>
-        </Auth.Provider>
+        <>
+            <Rainfall />
+            <Auth.Provider>
+                <Router>
+                    <Routes>
+                        <Route path="/level-select" element={<LevelSelectScreen />} />
+                        <Route path="/level/:level" element={<LevelScreen />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/signout" element={<SignOut />} />
+                        <Route path="/delete-account" element={<Delete />} />
+                        <Route path="*" element={<Navigate to="/level-select" />} />
+                    </Routes>
+                </Router>
+            </Auth.Provider>
+        </>
     );
 }
 
