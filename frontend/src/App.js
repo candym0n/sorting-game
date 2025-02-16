73500b3c3a5e054732420334c6579f3cd449da35
candym0n <jayden@bascio.com>
2025-02-16 14:57:29 +0800

    Removed matrix rainfall :(

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import React from 'react';
 function App() {
     return (
         <React.Fragment>
-            <Rainfall />
+            {/*<Rainfall />*/}
             <Auth.Provider>
                 <Router>
                     <Routes>
