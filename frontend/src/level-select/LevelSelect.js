db963ecfc03be5bab060c65946f96436a214493f
candym0n <jayden@bascio.com>
2025-02-28 11:03:44 +0800

    Added buttons for authentication

--- frontend/src/level-select/LevelSelect.js
+++ frontend/src/level-select/LevelSelect.js
@@ -1,9 +1,10 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router";
 import LevelSelectButton from "./LevelSelectButton";
 import Auth from "../auth/AuthContext";
-import { LogInIcon, LogOutIcon, DeleteIcon } from "lucide-react";
+import { LogInIcon, LogOutIcon, DeleteIcon, PlusCircleIcon } from "lucide-react";
 import { useContext, useEffect } from "react";
+import Login from "../auth/Login";
 
 export default function LevelSelect({ levels }) {
     const { data, setData } = useContext(Auth.Context);
@@ -25,6 +26,29 @@ export default function LevelSelect({ levels }) {
     return (
         <>
             <h1 style={titleStyle} className="text-center">Level Select</h1>
+            <div style={{ position: "absolute", right: 10, top: 10 }}>
+                {
+                    !data.logged_in ? (
+                        <>
+                            <Link to="/login">
+                                <LogInIcon size={100} />
+                            </Link>
+                            <Link to="/signup">
+                                <PlusCircleIcon size={100} />
+                            </Link>
+                        </>) : (
+                        <>
+                            <Link to="/delete-account">
+                                <DeleteIcon color="red" size={10} />
+                            </Link>
+                            <Link to="/signout">
+                                <LogOutIcon size={100} />
+                            </Link>
+                        </>
+                    )
+
+                }
+            </div>
             <Row className="g-4 p-5">
                 {levels.map((level) => (
                     <Col key={"level-select-" + level.index} sm={3} md={3} lg={2}>
